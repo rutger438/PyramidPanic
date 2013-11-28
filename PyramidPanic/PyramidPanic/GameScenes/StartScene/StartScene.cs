26b67c95a48b9b4e17ccfd01c8f02959f4c2e8a5
rutger438 <rutgerskateboard@gmail.com>
2013-11-28 14:06:32 +0100

    Aanpassingen gemaakt aan StartScene.cs

---
+++ PyramidPanic/PyramidPanic/GameScenes/StartScene/StartScene.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PyramidPanic
+{
+    public class StartScene
+    {
+        //Fields
+        private PyramidPanic game;
+        //Constructor
+        public StartScene(PyramidPanic game)
+        {
+            this.game = game;
+            this.Initialize();
+        }
+        //Initialise
+        protected void Initialize()
+        {
+
+        }
+        //LoadContent
+        public void LoadContent()
+        {
+
+        }
+        //Update
+        public void Update(GameTime gameTime)
+        {
+
+        }
+        //Draw
+        public void Draw(GameTime gameTime)
+        {
+
+        }
+    }
+}
